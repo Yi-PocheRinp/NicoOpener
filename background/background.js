ad99f6c0152675c656cf53ee95fd890f6983a802
tor4kichi <tor4kichi@hotmail.com>
2016-10-28 18:38:28 +0900

    アプリで開いた後に不要なタブを閉じる処理を追加

--- background/background.js
+++ background/background.js
@@ -1,23 +1,37 @@
 
-var NiocvideoContentIdRegex = /^https?:\/\/.*nicovideo.jp\/watch\/((sm|lv|so)\d*|\d*)/;
+const NiconicoScheme = "niconico://";
+const NiconicoPrefixList = ["sm", "lv", "so"];
+const NiconicoPrefixListRegexStr = NiconicoPrefixList.join("|");
+
+const NiocvideoContentIdRegex = new RegExp("https?:\\/\\/(?:www|live)\\.nicovideo.jp\\/watch\\/((?:"+NiconicoPrefixListRegexStr+")\\d*|\\d*)");
+//const NiocvideoContentIdRegex = /https?:\/\/(?:www|live)\.nicovideo.jp\/watch\/((?:sm|lv|so)\d*|\d*)/;
+const NiocvideoShortContentIdRegex = new RegExp("https?:\\/\\/nico.ms\\/((?:"+NiconicoPrefixListRegexStr+")\\d*|\\d*)");
+
+const TargetUrls = ["*://www.nicovideo.jp/*", "*://live.nicovideo.jp/*", "*://nico.ms/*"];
 
 function isNiconicoUrlWithContentId(url)
 {
-	return NiocvideoContentIdRegex.test(url);
+	return NiocvideoContentIdRegex.test(url) || NiocvideoShortContentIdRegex.text(url);
 }
 
 function extractNiconicoContentId(url)
 {
+	console.log(NiocvideoContentIdRegex);
 	var match = url.match(NiocvideoContentIdRegex);
 	console.log("match -> " + match);
 	if (match != null && match.length >= 2)
 	{
 		return match[1];
 	}
-	else 
+
+	match = url.match(NiocvideoShortContentIdRegex);
+	console.log("short match -> " + match);
+	if (match != null && match.length >= 2)
 	{
-		return null;
+		return match[1];
 	}
+	return null;
+	
 }
 
 function convertToNiconicoProtocolUrl(url)
@@ -25,13 +39,78 @@ function convertToNiconicoProtocolUrl(url)
 	var contentId = extractNiconicoContentId(url);
 	if (contentId != null)
 	{
-		return "niconico://" + contentId;
+		return NiconicoScheme + contentId;
 	}
 	else 
 	{
 		return null;
 	}
 }
+
+function isNiconicoSchemeUrl(url)
+{
+	return url.startsWith(NiconicoScheme);
+}
+
+
+function OpenNiconicoProtocol(niocnicoUrl)
+{
+	chrome.tabs.query({ currentWindow: true, active: true }, function (maybeCurrentTab) {
+		var currentTab = maybeCurrentTab[0];
+
+		// 新しいタブとして開く
+		// niconico://に対応するアプリを選択するよう表示されるはず
+		chrome.tabs.create({url: niocnicoUrl, active:false});
+
+		// カスタムスキームURLは tabs.create のコールバックが呼ばれない
+		// 少ししてから不要なタブを閉じる処理を実行する
+		setTimeout(function() {
+			// queryに 渡す url がカスタムスキームかつ
+			// host部分がないURLパターンの場合
+			// invalid patternとなり対応していない
+			// すべてのタブを取得して不要なタブを逐次判断する形で回避する 
+			chrome.tabs.query( {}, (tabs) => 
+			{
+					if (tabs && tabs.length)
+					{
+						for (var tabIndex in tabs)
+						{
+							var tab = tabs[tabIndex];
+							var isRemoveTarget = false;
+							if (tab.url && isNiconicoSchemeUrl(tab.url))
+							{
+								isRemoveTarget = true;
+							}
+							else if (currentTab.index < tabIndex)
+							{
+								// httpとしてレンダリングが実行されてないページは
+								// tab.urlがabout:blankのままっぽい (firefoxで確認)
+								// 現在のタブより新しく開いたっぽい空ページタブを対象として判断
+								if (tab.url && tab.url == "about:blank")
+								{
+									isRemoveTarget = true;
+								}
+							}
+							
+							if (isRemoveTarget)
+							{
+								chrome.tabs.remove(tab.id, () => 
+								{
+									if (chrome.runtime.lastError) {
+										console.log("failed: " + chrome.runtime.lastError);
+									} else {
+										console.log("removed tab");
+									}
+								});
+							}								
+						}
+					}
+			});
+		}, 500);
+	});
+	
+}
+
 
 /*
 ニコニコ動画で開くためのコンテキストメニュー
@@ -40,19 +119,16 @@ chrome.contextMenus.create({
   id: "play",
   title: chrome.i18n.getMessage("contextMenuItemPlay"),
   contexts: ["link"],
-	targetUrlPatterns:["*://*.nicovideo.jp/*"],
+	targetUrlPatterns: TargetUrls,
   onclick : (info, tab) => 
 	{
-		console.log(info.linkUrl);
+		console.log(info);
 		var niconicoUri = convertToNiconicoProtocolUrl(info.linkUrl);
 		if (niconicoUri != null)
 		{
 			console.log("[PLAY] " + niconicoUri);
 
-			chrome.tabs.create({url: niconicoUri}, (tab) => {
-				chrome.tabs.remove(tab.id);
-			});
-
+			OpenNiconicoProtocol(niconicoUri);
 		}
 	}
 });
@@ -60,7 +136,7 @@ chrome.contextMenus.create({
 chrome.contextMenus.create({
   id: "separator",
   contexts: ["link"],
-	targetUrlPatterns:["*://*.nicovideo.jp/*"],
+	targetUrlPatterns:TargetUrls,
   type: "separator"
 });
 
@@ -68,7 +144,7 @@ chrome.contextMenus.create({
   id: "add-playlist",
   title: chrome.i18n.getMessage("contextMenuItemNicoAddToPlaylist"),
   contexts: ["link"],
-	targetUrlPatterns:["*://*.nicovideo.jp/*"],
+	targetUrlPatterns:TargetUrls,
   onclick : (info, tab) => 
 	{
 		console.log(info.linkUrl);
@@ -78,10 +154,7 @@ chrome.contextMenus.create({
 			var niconicoUriWithAddPlaylist = niconicoUri + "?addplaylist=1"; 
 			console.log("[Add Playlist] " + niconicoUriWithAddPlaylist);
 
-			chrome.tabs.create({url: niconicoUriWithAddPlaylist}, (tab) => {
-				chrome.tabs.remove(tab.id);
-			});
-
+			OpenNiconicoProtocol(niconicoUriWithAddPlaylist);
 		}
 	}
 });
